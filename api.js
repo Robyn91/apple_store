ac35f54aeed254cf043c6dbfc6d205c0c592b523
Robyn <robynlindsay12@gmail.com>
2022-08-05 10:57:49 +0200

    removed helmet for deployment

--- api.js
+++ api.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const fileHandler = require('fs');
-const helmet = require("helmet");
 const cors = require('cors');
 const path = require("path");
 const app = express();
-app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
